9c6502aa6c75655eb9f24b6eeff30071c0ff32ba
duncan <duncankithinji91@gmail.com>
2021-12-10 12:34:53 +0300

    using interfaces to describe data

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
-import { s } from "@angular/core/src/render3";
 import { ProductService } from "../product.service";
+import { Album } from "../album";
 
 @Component({
   selector: "app-product-description",
@@ -8,13 +8,13 @@ import { ProductService } from "../product.service";
   styleUrls: ["./product-description.component.css"],
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  albumInfo: Album;
 
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
     this._productService
-    .getAlbum(1)
-    .subscribe(response => this.albumInfo = response);
+      .getAlbum(1)
+      .subscribe(response => this.albumInfo = response);
   }
 }
